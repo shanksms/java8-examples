1e97663a6782e3efc578e3fbbfa0f5bd8dbd1f90
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-08-15 22:11:09 +0530

    more program

---
+++ src/main/java/com/shashank/completablefutures/CompletableFutureWithSupplier.java
@@ -0,0 +1,15 @@
+package com.shashank.completablefutures;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class CompletableFutureWithSupplier {
+    public static void main(String[] args) {
+        Supplier<String> supplier = () -> Thread.currentThread().getName();
+
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier);
+        String string = completableFuture.join();
+        System.out.println("result " + string);
+
+    }
+}
